692ded374425e815df94bbb253422aae7347d700
mirunamovi <mirunamovi@gmail.com>
2023-07-11 12:29:26 +0300

    Changes for delete

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,5 +18,10 @@ export class TasksComponent {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+
+  }
+
 
 }
