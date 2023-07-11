e23a8a0b2207cc1cd50d6006174b856ee8cf8d7c
mirunamovi <mirunamovi@gmail.com>
2023-07-11 14:47:40 +0300

    Toggle Reminder

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -23,5 +23,10 @@ export class TasksComponent {
 
   }
 
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { Task } from '../Task';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+    })
+};
+ 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,5 +24,11 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+
   }
 }
